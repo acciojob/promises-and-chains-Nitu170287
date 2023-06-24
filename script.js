c65623eb0bd085b11e3d33adf60dd223a1f81744
Nitu Dubey <108494260+Nitu170287@users.noreply.github.com>
2023-06-24 04:08:57 -0400

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+let name1 = document.getElementById("name").value
+let age1 = document.getElementById("age").value
+
+document.getElementById("btn").addEventListener("click",()=>{
+	console.log("btn is clicked")
+	checkAge(name, age)
+  .then(message => {
+    alert(message);
+  })
+  .catch(errorMessage => {
+    alert(errorMessage);
+  });
+})
+
+
+function checkAge(name1, age1) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (age >= 18) {
+        resolve(`Welcome, ${name}. You can vote.`);
+      } else {
+        reject(`Oh sorry ${name}. You aren't old enough.`);
+      }
+    }, 4000);
+  });
+}
